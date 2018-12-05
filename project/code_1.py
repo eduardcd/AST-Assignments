9d284a61eb22af94e6907c1c12c32ef316bd0831
Vahid MohammadiGahrooei <vahmohh@gmail.com>
2018-12-05 22:59:19 +0100

    fix empty list bug

--- project/code_1.py
+++ project/code_1.py
@@ -3,17 +3,27 @@ def takeSecond(elem):
 
 class Data:
     def __init__(self, rgbd, rgb):
-        if len(rgbd) != len(rgb):
-            raise ValueError('Invalid Data')
-        for i in range(len(rgbd)):
-            if len(rgbd[i]) != len(rgb[i]):
+        if len(rgbd) != 0 and len(rgb) != 0:
+            if len(rgbd) != len(rgb):
                 raise ValueError('Invalid Data')
-        self._names = [i[0] for i in sorted(rgbd, key=lambda x: x[0])]
-        self._ids = [i[1] for i in sorted(rgbd, key=lambda x: x[0])]
-        self._rgbd = [i[2] for i in sorted(rgbd, key=lambda x: x[0])]
-        self._rgb = [i[2] for i in sorted(rgb, key=lambda x: x[0])]
+            for i in range(len(rgbd)):
+                if len(rgbd[i]) != len(rgb[i]):
+                    raise ValueError('Invalid Data')
+            self._names = [i[0] for i in sorted(rgbd, key=lambda x: x[0])]
+            self._ids = [i[1] for i in sorted(rgbd, key=lambda x: x[0])]
+            self._rgbd = [i[2] for i in sorted(rgbd, key=lambda x: x[0])]
+            self._rgb = [i[2] for i in sorted(rgb, key=lambda x: x[0])]
+        else:
+            self._rgbd = sorted(rgbd)
+            self._rgb = sorted(rgb)
         
     def calculate(self):
+        if len(self._rgbd) == 0 and len(self._rgb) == 0:
+            return []
+        if len(self._rgbd) == 0:
+            return self._rgb
+        if len(self._rgb) == 0:
+            return self._rgbd
         lista = []
         for i in range(len(self._names)):
             value = (self._names[i], self._ids[i], max(self._rgbd[i], self._rgb[i]))
